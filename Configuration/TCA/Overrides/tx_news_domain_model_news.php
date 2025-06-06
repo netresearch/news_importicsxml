999832b9f8db857d3681216636cc2400bfd7a80b
Rico Sonntag <rico.sonntag@netresearch.de>
2025-06-06 10:03:34 +0000

    Update TCA override

--- Configuration/TCA/Overrides/tx_news_domain_model_news.php
+++ Configuration/TCA/Overrides/tx_news_domain_model_news.php
@@ -11,19 +11,24 @@ declare(strict_types=1);
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-$fields = [
-    'news_import_data' => [
-        'exclude' => true,
-        'label'   => 'LLL:EXT:news_importicsxml/Resources/Private/Language/locallang.xlf:tx_news_domain_model_news.news_import_data',
-        'config'  => [
-            'type'       => 'text',
-            'cols'       => 60,
-            'rows'       => 20,
-            'readOnly'   => true,
-            'renderType' => 'json',
+ExtensionManagementUtility::addTCAcolumns(
+    'tx_news_domain_model_news',
+    [
+        'news_import_data' => [
+            'exclude' => true,
+            'label'   => 'LLL:EXT:news_importicsxml/Resources/Private/Language/locallang.xlf:tx_news_domain_model_news.news_import_data',
+            'config'  => [
+                'type'       => 'text',
+                'cols'       => 60,
+                'rows'       => 20,
+                'readOnly'   => true,
+                'renderType' => 'json',
+            ],
         ],
-    ],
-];
+    ]
+);
 
-ExtensionManagementUtility::addTCAcolumns('tx_news_domain_model_news', $fields);
-ExtensionManagementUtility::addToAllTCAtypes('tx_news_domain_model_news', 'news_import_data');
+ExtensionManagementUtility::addToAllTCAtypes(
+    'tx_news_domain_model_news',
+    'news_import_data'
+);
