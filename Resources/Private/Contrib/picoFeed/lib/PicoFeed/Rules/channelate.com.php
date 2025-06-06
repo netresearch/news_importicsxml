7b36c2a1654bbfe574cd6fb7da132dfb0e4e6df4
Rico Sonntag <rico.sonntag@netresearch.de>
2025-06-06 14:22:12 +0000

    Remove contrib directory

--- Resources/Private/Contrib/picoFeed/lib/PicoFeed/Rules/channelate.com.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'filter' => [
-        '%.*%' => [
-            '%href="http://www.channelate.com/(\\d+)/(\\d+)/(\\d+)/[^"]*"%' => 'href="http://www.channelate.com/extra-panel/$1$2$3/"'
-        ]
-    ]
-];
