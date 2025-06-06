8a8e68dd9caa674e13e0743e0ee1bb3df6d050b4
Rico Sonntag <rico.sonntag@netresearch.de>
2025-06-06 12:17:26 +0000

    Fix unittests

--- Tests/Unit/Domain/Model/Dto/TaskConfigurationTest.php
+++ Tests/Unit/Domain/Model/Dto/TaskConfigurationTest.php
@@ -11,84 +11,69 @@ declare(strict_types=1);
 
 namespace GeorgRinger\NewsImporticsxml\Tests\Unit\Domain\Model\Dto;
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
-
 use GeorgRinger\NewsImporticsxml\Domain\Model\Dto\TaskConfiguration;
-use TYPO3\CMS\Core\Tests\UnitTestCase;
+use PHPUnit\Framework\Attributes\Test;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 
 class TaskConfigurationTest extends UnitTestCase
 {
     /**
      * @var TaskConfiguration
      */
-    protected $instance;
+    protected TaskConfiguration $instance;
 
     /**
      * Setup.
      */
-    protected function setUp()
+    protected function setUp(): void
     {
+        parent::setUp();
+
         $this->instance = new TaskConfiguration();
     }
 
-    /**
-     * @test
-     */
+    #[Test]
     public function emailCanBeSet(): void
     {
         $value = 'fo@bar.com';
         $this->instance->setEmail($value);
-        $this->assertEquals($value, $this->instance->getEmail());
+
+        self::assertEquals($value, $this->instance->getEmail());
     }
 
-    /**
-     * @test
-     */
+    #[Test]
     public function pathCanBeSet(): void
     {
         $value = 'fileadmin/123.xml';
         $this->instance->setPath($value);
-        $this->assertEquals($value, $this->instance->getPath());
+
+        self::assertEquals($value, $this->instance->getPath());
     }
 
-    /**
-     * @test
-     */
+    #[Test]
     public function formatCanBeSet(): void
     {
         $value = 'xml';
         $this->instance->setFormat($value);
-        $this->assertEquals($value, $this->instance->getFormat());
+
+        self::assertEquals($value, $this->instance->getFormat());
     }
 
-    /**
-     * @test
-     */
+    #[Test]
     public function pidCanBeSet(): void
     {
-        $value = '456';
+        $value = 456;
         $this->instance->setPid($value);
-        $this->assertEquals($value, $this->instance->getPid());
+
+        self::assertEquals($value, $this->instance->getPid());
     }
 
-    /**
-     * @test
-     */
+    #[Test]
     public function mappingCanBeSet(): void
     {
         $value = 'fo:bar';
         $this->instance->setMapping($value);
-        $this->assertEquals($value, $this->instance->getMapping());
+
+        self::assertEquals($value, $this->instance->getMapping());
     }
 }
--- Tests/Unit/Jobs/ImportJobTest.php
+++ Tests/Unit/Jobs/ImportJobTest.php
@@ -11,86 +11,106 @@ declare(strict_types=1);
 
 namespace GeorgRinger\NewsImporticsxml\Tests\Unit\Jobs;
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
-use GeorgRinger\NewsImporticsxml\Mapper\XmlMapper;
-use GeorgRinger\NewsImporticsxml\Mapper\IcsMapper;
 use GeorgRinger\NewsImporticsxml\Domain\Model\Dto\TaskConfiguration;
 use GeorgRinger\NewsImporticsxml\Jobs\ImportJob;
-use TYPO3\CMS\Core\Log\Logger;
-use TYPO3\CMS\Core\Tests\UnitTestCase;
+use GeorgRinger\NewsImporticsxml\Mapper\IcsMapper;
+use GeorgRinger\NewsImporticsxml\Mapper\XmlMapper;
+use PHPUnit\Framework\Attributes\Test;
+use Psr\Log\NullLogger;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+use UnexpectedValueException;
 
 class ImportJobTest extends UnitTestCase
 {
-    protected $mockedJob;
+    /**
+     * @var ImportJob
+     */
+    protected ImportJob $mockedJob;
 
     public function setUp(): void
     {
-        $logger = $this->getAccessibleMock(Logger::class, ['dummy'], [], '', false);
+        parent::setUp();
 
-        $this->mockedJob = $this->getAccessibleMock(ImportJob::class, ['import'],
-            [], '', false);
-        $this->mockedJob->_set('logger', $logger);
+        $this->mockedJob = $this->getAccessibleMock(
+            ImportJob::class,
+            ['import'],
+            [],
+            '',
+            false
+        );
+
+        $this->mockedJob->_set(
+            'logger',
+            new NullLogger()
+        );
     }
 
-    /**
-     * @test
-     */
+    #[Test]
     public function xmlMapperIsCalledWithXmlConfiguration(): void
     {
         $configuration = new TaskConfiguration();
         $configuration->setFormat('xml');
 
-        $this->mockedJob->_set('configuration', $configuration);
+        $xmlMapper = $this->getAccessibleMock(
+            XmlMapper::class,
+            ['map'],
+            [],
+            '',
+            false
+        );
 
-        $xmlMapper = $this->getAccessibleMock(XmlMapper::class, ['map']);
-        $this->mockedJob->_set('xmlMapper', $xmlMapper);
+        $xmlMapper
+            ->expects($this->once())
+            ->method('map');
 
-        $xmlMapper->expects($this->once())->method('map');
+        $this->mockedJob->_set(
+            'xmlMapper',
+            $xmlMapper
+        );
 
-        $this->mockedJob->_call('run');
+        $this->mockedJob
+            ->setConfiguration($configuration)
+            ->run();
     }
 
-    /**
-     * @test
-     */
+    #[Test]
     public function icsMapperIsCalledWithXmlConfiguration(): void
     {
         $configuration = new TaskConfiguration();
         $configuration->setFormat('ics');
 
-        $this->mockedJob->_set('configuration', $configuration);
+        $icsMapper = $this->getAccessibleMock(
+            IcsMapper::class,
+            ['map'],
+            [],
+            '',
+            false
+        );
 
-        $icsMapper = $this->getAccessibleMock(IcsMapper::class, ['map']);
-        $this->mockedJob->_set('icsMapper', $icsMapper);
+        $icsMapper
+            ->expects($this->once())
+            ->method('map');
 
-        $icsMapper->expects($this->once())->method('map');
+        $this->mockedJob->_set(
+            'icsMapper',
+            $icsMapper
+        );
 
-        $this->mockedJob->_call('run');
+        $this->mockedJob
+            ->setConfiguration($configuration)
+            ->run();
     }
 
-    /**
-     * @test
-     *
-     * @expectedException \UnexpectedValueException
-     */
+    #[Test]
     public function nonSupportedMapperThrowsException(): void
     {
+        $this->expectException(UnexpectedValueException::class);
+
         $configuration = new TaskConfiguration();
         $configuration->setFormat('fo');
 
-        $this->mockedJob->_set('configuration', $configuration);
-
-        $this->mockedJob->_call('run');
+        $this->mockedJob
+            ->setConfiguration($configuration)
+            ->run();
     }
 }
